9830a3b1b4f831305be42adaacfd7070f5d2ab9b
no name <ginobilinie@users.noreply.github.com>
2018-07-09 15:41:02 +0800

    Update nnBuildUnits.py

--- nnBuildUnits.py
+++ nnBuildUnits.py
@@ -1495,57 +1495,19 @@ class DiceLoss4Organs(Function):
 
 '''
 GDL loss for the image reconstruction
+Note, this copy of gdl loss is successful. It may not be able to pass the gradcheck, that's becuase of the float/double issue, but it is fine.
+If we change the filter to torch.DoubleTensor, and also cast the pred and gt as double tensor, it will pass the grad check. However, float is fine 
+with the real application.
 By Dong Nie
-'''
-
-class gdl_loss(nn.Module):
-    def __init__(self, pNorm=2):
-        super(gdl_loss, self).__init__()
-        self.convX = nn.Conv2d(1, 1, kernel_size=(1, 2), stride=1, padding=(0, 1), bias=False)
-        self.convY = nn.Conv2d(1, 1, kernel_size=(2, 1), stride=1, padding=(1, 0), bias=False)
-
-        filterX = torch.FloatTensor([[[[-1, 1]]]])  # 1x2
-        filterY = torch.FloatTensor([[[[1], [-1]]]])  # 2x1
-        filterX = Variable(filterX, requires_grad=False)
-        filterY = Variable(filterY, requires_grad=False)
-        self.convX.weight = torch.nn.Parameter(filterX)
-        self.convY.weight = torch.nn.Parameter(filterY)
-        self.pNorm = Variable(torch.FloatTensor(1).fill_(pNorm))
-
-    def forward(self, pred, gt):
-        assert not gt.requires_grad
-        assert pred.dim() == 4
-        assert gt.dim() == 4
-        assert pred.size() == gt.size(), "{0} vs {1} ".format(pred.size(), gt.size())
-
-        pred_dx = torch.abs(self.convX(pred))
-        pred_dy = torch.abs(self.convY(pred))
-        gt_dx = torch.abs(self.convX(gt))
-        gt_dy = torch.abs(self.convY(gt))
-
-        grad_diff_x = torch.abs(gt_dx - pred_dx)
-        grad_diff_y = torch.abs(gt_dy - pred_dy)
-
-        # print 'grad_diff_x.shape: ', grad_diff_x.shape, ' grad_diff_y.shape: ', grad_diff_y.shape
-
-        mat_loss_x = grad_diff_x ** self.pNorm
-
-        mat_loss_y = grad_diff_y ** self.pNorm  # Batch x Channel x width x height
-
-        shape = gt.shape
-
-        mean_loss = (torch.sum(mat_loss_x) + torch.sum(mat_loss_y)) / (shape[0] * shape[1] * shape[2] * shape[3])
-
-        return mean_loss
-    
-class gdl_loss1(nn.Module):   
+'''    
+class gdl_loss(nn.Module):   
     def __init__(self, pNorm=2):
         super(gdl_loss1, self).__init__()
         self.convX = nn.Conv2d(1, 1, kernel_size=(1, 2), stride=1, padding=(0, 1), bias=False)
         self.convY = nn.Conv2d(1, 1, kernel_size=(2, 1), stride=1, padding=(1, 0), bias=False)
         
         filterX = torch.FloatTensor([[[[-1, 1]]]])  # 1x2
-        filterY = torch.FloatTensor([[[[1], [-1]]]])  # 2x1
+        filterY = torch.FloatTensor([[[[1], [-1]]]])  # 2x1 
         
         self.convX.weight = torch.nn.Parameter(filterX,requires_grad=False)
         self.convY.weight = torch.nn.Parameter(filterY,requires_grad=False)
@@ -1570,8 +1532,10 @@ class gdl_loss1(nn.Module):
         
         mat_loss_y = grad_diff_y ** self.pNorm  # Batch x Channel x width x height
         
-        mean_loss=torch.sum(mat_loss_x+mat_loss_y)
-        
+        shape = gt.shape
+
+        mean_loss = (torch.sum(mat_loss_x) + torch.sum(mat_loss_y)) / (shape[0] * shape[1] * shape[2] * shape[3]) 
+               
         return mean_loss
     
     
